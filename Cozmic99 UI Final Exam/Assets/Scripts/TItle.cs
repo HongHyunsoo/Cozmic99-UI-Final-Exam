21ab9d170b756f9498032ef6c69cb2ebfc60dadd
HongHyunsoo <cuoxote@gmail.com>
2025-06-08 22:07:21 +0900

    20250608 [타이틀 완성]

---
+++ Cozmic99 UI Final Exam/Assets/Scripts/TItle.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonHandler : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public Animator ScreenAnimator;
+    public GameObject TItleTexts;
+
+    public GameObject Screen;
+    public GameObject Screen_Setting;
+    public GameObject Screen_Credit;
+    public GameObject Screen_Save;
+
+    public void OnClick()
+    {
+        
+    }
+    public void OnStartButtonClicked()
+    {
+        if (doorAnimator != null && ScreenAnimator != null)
+        {
+            StartCoroutine(DelayedThing());
+            doorAnimator.SetBool("Is Open?", true); //  Bool true로 설정
+            TItleTexts.SetActive(false);  // 여기서 꺼짐
+            ScreenAnimator.SetBool("Is ScreenOpen?", true); //  Bool true로 설정
+            
+            IEnumerator DelayedThing()
+            {
+                yield return new WaitForSeconds(1.0f);
+                Screen.SetActive(true);
+                Screen_Save.SetActive(true);
+            }
+
+            
+        }
+    }
+
+    public void OnSettingButtonClicked()
+    {
+        if (doorAnimator != null && ScreenAnimator != null)
+        {
+            StartCoroutine(DelayedThing());
+            doorAnimator.SetBool("Is Open?", true); //  Bool true로 설정
+            TItleTexts.SetActive(false);  // 여기서 꺼짐
+            ScreenAnimator.SetBool("Is ScreenOpen?", true); //  Bool true로 설정
+
+            IEnumerator DelayedThing()
+            {
+                yield return new WaitForSeconds(1.0f);
+                Screen.SetActive(true);
+                Screen_Setting.SetActive(true);
+            }
+
+
+        }
+    }
+
+    public void OnCreditButtonClicked()
+    {
+        if (doorAnimator != null && ScreenAnimator != null)
+        {
+            StartCoroutine(DelayedThing());
+            doorAnimator.SetBool("Is Open?", true); //  Bool true로 설정
+            TItleTexts.SetActive(false);  // 여기서 꺼짐
+            ScreenAnimator.SetBool("Is ScreenOpen?", true); //  Bool true로 설정
+
+            IEnumerator DelayedThing()
+            {
+                yield return new WaitForSeconds(1.0f);
+                Screen.SetActive(true);
+                Screen_Credit.SetActive(true);
+            }
+
+
+        }
+    }
+
+    public void OnExitButtenClicked()
+    {
+        if (doorAnimator != null && ScreenAnimator != null)
+        {
+            doorAnimator.SetBool("Is Open?", false); //  Bool false로 설정
+            ScreenAnimator.SetBool("Is ScreenOpen?", false); //  Bool false로 설정
+            TItleTexts.SetActive(true);  // 여기서 켜짐
+            Screen_Setting.SetActive(false);
+            Screen_Credit.SetActive(false);
+            Screen_Save.SetActive(false);
+        }
+    }
+}
